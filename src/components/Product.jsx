b7cda7e9a19b1c97fc1448f85dc54d9f7222e055
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-06-26 20:36:58 -0400

    adding option to remove item from cart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useAddProduct } from '../recoil/hooks';
+import { useAddProduct, useRemoveProduct } from '../recoil/hooks';
 
 const Product = ({ product }) => {
   const addProduct = useAddProduct();
+  const removeProduct = useRemoveProduct();
   
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <p>{product.title}</p>
       <p>{product.price}</p>
       <button onClick={() => addProduct(product)}>Add to cart</button>
+      <button onClick={() => removeProduct(product)}>Remove from cart</button>
     </div>
   )
 }
